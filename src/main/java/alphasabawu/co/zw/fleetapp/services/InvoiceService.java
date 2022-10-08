617a7c34444c206e479fa2b1a91332307585b39f
alpha sabawu <46676904+al-zero@users.noreply.github.com>
2022-10-08 11:27:45 +0200

    fifth commit

--- src/main/java/alphasabawu/co/zw/fleetapp/services/InvoiceService.java
+++ src/main/java/alphasabawu/co/zw/fleetapp/services/InvoiceService.java
@@ -1,8 +1,35 @@
 package alphasabawu.co.zw.fleetapp.services;
 
 
+import alphasabawu.co.zw.fleetapp.models.Invoice;
+import alphasabawu.co.zw.fleetapp.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
+    
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+    // Get Invoices
+    public List<Invoice> getInvoices(){
+        return invoiceRepository.findAll();
+    }
+    // Add Invoices
+    public void saveInvoice(Invoice invoice){
+        invoiceRepository.save(invoice);
+    }
+
+    public Optional<Invoice> getInvoiceByID(int id) {
+        return invoiceRepository.findById(id);
+    }
+
+    public void removeInvoice(Integer id) {
+        invoiceRepository.deleteById(id);
+    }
+    
+    
 }
